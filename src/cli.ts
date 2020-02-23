6f2caec390af79f8bdde836ebdac2df1903010c6
jeanfortheweb <jeanfortheweb@gmail.com>
2020-02-23 14:18:42 +0100

    Chore: Refactor cli code into several functions

--- src/cli.ts
+++ src/cli.ts
@@ -1,12 +1,140 @@
 #!/usr/bin/env node
 import "ts-node/register";
-import program, { Command } from "commander";
+import program from "commander";
 import { sync } from "glob";
 import { relative } from "path";
 import { createConnection, Connection, ConnectionOptionsReader } from "typeorm";
-import ora from "ora";
+import ora, { Ora } from "ora";
 import { collect } from "./collect";
 import { install } from "./install";
+
+interface InstallCommand {
+  connection?: string;
+  resetDatabase: boolean;
+  useMigrations: boolean;
+}
+
+function collectBundles(spinner: Ora, pattern: string): [string, any[]][] {
+  spinner.start("Collecting fixtures from bundles");
+
+  const bundles = sync(pattern, {
+    cwd: process.cwd(),
+    absolute: true
+  }).map<[string, any[]]>(file => {
+    try {
+      return [relative(process.cwd(), file), collect(require(file))];
+    } catch (error) {
+      spinner.fail(
+        `Failed to load bundle file at: ${relative(process.cwd(), file)}: ${
+          error.message
+        }`
+      );
+
+      return process.exit(1);
+    }
+  });
+
+  const count = bundles.reduce(
+    (count, fixtures) => (count += fixtures.length),
+    0
+  );
+
+  spinner.succeed(
+    `Found ${bundles.length} fixture bundles with a total of ${count} fixtures`
+  );
+
+  return bundles;
+}
+
+async function getConnection(spinner: Ora, connectionName: string) {
+  let connection: Connection;
+
+  try {
+    spinner.start("Connecting to database");
+
+    try {
+      connection = await createConnection(
+        await new ConnectionOptionsReader({
+          root: process.cwd()
+        }).get(connectionName)
+      );
+    } catch (error) {
+      connection = await createConnection(connectionName);
+    }
+  } catch (error) {
+    spinner.fail(`Failed to get database connection: ${error.message}`);
+
+    return process.exit(1);
+  }
+
+  spinner.succeed("Connected");
+
+  return connection;
+}
+
+async function reset(
+  spinner: Ora,
+  connection: Connection,
+  useMigrations: boolean
+) {
+  try {
+    await connection.dropDatabase();
+
+    if (useMigrations === true) {
+      spinner.start("Migrating database");
+
+      await connection.runMigrations();
+    } else {
+      spinner.start("Synchronizing database");
+
+      await connection.synchronize();
+    }
+  } catch (error) {
+    spinner.fail(`Failed to reset database: ${error.message}`);
+
+    return process.exit(1);
+  }
+
+  spinner.succeed("Database reset complete");
+}
+
+async function installBundles(
+  spinner: Ora,
+  connection: Connection,
+  bundles: [string, any[]][]
+) {
+  for (const [path, fixtures] of bundles) {
+    spinner.start(path);
+
+    await install(connection, fixtures);
+
+    spinner.succeed(path);
+  }
+}
+
+async function action(
+  pattern: string = "./fixtures/**/*.bundle.ts",
+  options: InstallCommand
+) {
+  const {
+    connection: connectionName = "default",
+    resetDatabase,
+    useMigrations
+  } = options;
+
+  const spinner = ora("");
+  const bundles = collectBundles(spinner, pattern);
+  const connection = await getConnection(spinner, connectionName);
+
+  if (resetDatabase === true) {
+    await reset(spinner, connection, useMigrations);
+  }
+
+  await installBundles(spinner, connection, bundles);
+
+  spinner.succeed("Done");
+  process.exit(0);
+}
 
 program
   .command("install [pattern]")
@@ -26,100 +154,6 @@ program
     "-m, --use-migrations",
     "Execute migrations instead synchronization after dropping the database"
   )
-  .action(
-    async (
-      pattern: string = "./fixtures/**/*.bundle.ts",
-      {
-        connection: connectionName = "default",
-        resetDatabase,
-        useMigrations
-      }: Command
-    ) => {
-      const spinner = ora("");
-
-      spinner.start("Collecting fixtures from bundles");
-
-      const bundles = sync(pattern, {
-        cwd: process.cwd(),
-        absolute: true
-      }).map<[string, any[]]>(file => {
-        try {
-          return [relative(process.cwd(), file), collect(require(file))];
-        } catch (error) {
-          spinner.fail(
-            `Failed to load bundle file at: ${relative(process.cwd(), file)}: ${
-              error.message
-            }`
-          );
-
-          return process.exit(1);
-        }
-      });
-
-      const count = bundles.reduce(
-        (count, fixtures) => (count += fixtures.length),
-        0
-      );
-
-      spinner.succeed(
-        `Found ${bundles.length} fixture bundles with a total of ${count} fixtures`
-      );
-
-      let connection: Connection;
-
-      try {
-        spinner.start("Connecting to database");
-
-        try {
-          connection = await createConnection(
-            await new ConnectionOptionsReader({
-              root: process.cwd()
-            }).get(connectionName)
-          );
-        } catch (error) {
-          connection = await createConnection(connectionName);
-        }
-      } catch (error) {
-        spinner.fail(`Failed to get database connection: ${error.message}`);
-
-        return process.exit(1);
-      }
-
-      spinner.succeed("Connected");
-
-      if (resetDatabase === true) {
-        try {
-          await connection.dropDatabase();
-
-          if (useMigrations === true) {
-            spinner.start("Migrating database");
-
-            await connection.runMigrations();
-          } else {
-            spinner.start("Synchronizing database");
-
-            await connection.synchronize();
-          }
-        } catch (error) {
-          spinner.fail(`Failed to reset database: ${error.message}`);
-
-          return process.exit(1);
-        }
-
-        spinner.succeed("Database reset complete");
-      }
-
-      for (const [path, fixtures] of bundles) {
-        spinner.start(path);
-
-        await install(connection, fixtures);
-
-        spinner.succeed(path);
-      }
-
-      spinner.succeed("Done");
-      process.exit(0);
-    }
-  );
+  .action(action);
 
 program.parse(process.argv);
